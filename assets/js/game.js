5100d989b58020cf8fc99fb7e7b9902cf998bfda
ryanosaurus-tex <rmw.inbox@gmail.com>
2017-08-12 18:29:12 -0500

    random word selector working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,8 @@
 
-var buttonModule = {
+/*
+var ButtonModule = {
     alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    createButtons : function() {
+    createButtons: function() {
   	    var i = 0;
   	    for (i = 0; i < alphabet.length; i++) {
   	        var button = document.createElement("BUTTON");
@@ -10,4 +11,22 @@ var buttonModule = {
   	        document.getElementById("character-pool").appendChild(button);
   	    } // close for(i) 	
     }, // close createButtons()
-} // close buttonModule
+} // close ButtonModule
+*/
+
+
+var WordModule = {
+	wordBank: ["Bluebird", "Mockingbird", "Cardinal", "Robin", "Chickadee", "Grackle", "Dove"],	
+	wordSelector: function() {		
+		var x = Math.floor(Math.random() * (this.wordBank.length) );
+		return this.wordBank[x];		
+	}
+
+}; // close WordModule
+
+console.log( WordModule.wordSelector() );
+
+
+
+
+		
