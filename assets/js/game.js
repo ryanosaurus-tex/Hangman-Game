3fa77355e3a2e78a1a4cd2af12f9d36f48d7f12f
ryanosaurus-tex <rmw.inbox@gmail.com>
2017-08-13 19:37:39 -0500

    word generator working, key recorder working, alphabet buttons working (unstyled)

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,25 @@
 
-/*
-var ButtonModule = {
+
+var buttonModule = {
     alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
     createButtons: function() {
   	    var i = 0;
-  	    for (i = 0; i < alphabet.length; i++) {
+  	    for (i = 0; i < this.alphabet.length; i++) {
   	        var button = document.createElement("BUTTON");
-  	        var text = document.createTextNode(alphabet[i]);
+  	        var text = document.createTextNode(buttonModule.alphabet[i]);
   	        button.appendChild(text);
   	        document.getElementById("character-pool").appendChild(button);
-  	    } // close for(i) 	
-    }, // close createButtons()
-} // close ButtonModule
+  	    };  	
+    } 
+} 
+
+/*
+alphabet: [
+      "A", "B", "C", "D", "E", "F", "G",
+      "H", "I", "J", "K", "L", "M", "N",
+      "O", "P", "Q", "R", "S", "T", "U",
+      "V", "W", "X", "Y", "Z"
+      ],
 */
 
 
@@ -39,13 +47,45 @@ var wordModule = {
 
 }; // close WordModule
 
-console.log(wordModule.wordLoader());
+var gameModule = {
+  gameWord: wordModule.wordLoader(),
+  gameKeyHandler: function(event) {
+    var userKey = event.key;
+    console.log(userKey);
+
+    // check to see if userKey is in gameWord using test()
 
 
+      // if true:
+
+        // run a RegExp/g to search() for and return positions of match -> save in var X
+
+        // run a RegExp/g to replace() glyphicon ?s with userKey characters
+
+      // OR if true (alternate):
+
+        // create for loop that goes through every character in gameWord and checks if:
+        // userKey === gameword[i]
+
+      // if false:
+
+        // subtract 1 from attempts remaining counter
+
+    // change opacity on word-bank buttons
+
+  }
+  
 
 
+}
+
+console.log(gameModule.gameWord);
 
 
+// add event listener
 
+document.addEventListener("keyup", gameModule.gameKeyHandler);
+
+document.body.onload = buttonModule.createButtons();
 
 		
